162a6074d412341eb2508586ae64bb9d465451e8
Kunal Biraris <birariskunalqa@gmail.com>
2026-04-22 10:28:47 +0530

    created json file to acheive data driven testing

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,11 +1,12 @@
 import {test, expect} from '@playwright/test';
 import { LoginPage } from './pages/loginPage';
+import data from './testData/orangeHrmLoginData.json';
 
 test("Login test with valid credentials", async ({page}) => {
 
     const loginPage = new LoginPage(page);
     await loginPage.b_navigateTo('https://opensource-demo.orangehrmlive.com/');
-    await loginPage.enterUsername('Admin');
-    await loginPage.enterPassword('admin123');
+    await loginPage.enterUsername(data.validUsername);
+    await loginPage.enterPassword(data.validPassword);
     await loginPage.clickLogin();
 });
